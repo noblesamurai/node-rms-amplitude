25ba9c109b1c188e4f731bc8859e9594d7331849
Tim Allen <tim@noblesamurai.com>
2019-07-24 12:40:01 +1000

    Ability to specify paths for ffmpeg/sox

--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 const tempy = require('tempy');
 
-module.exports = async function sox (filename) {
+/**
+ * @param {string} filename
+ * @param {object} opts
+ * @param {string} opts.ffmpeg ffmpeg full path
+ * @param {string} opts.sox sox full path
+ */
+module.exports = async function sox (filename, opts) {
+  const { ffmpeg = 'ffmpeg', sox = 'sox' } = opts;
   const tempfile = tempy.file({ extension: 'wav' });
-  await exec(`ffmpeg -i ${filename} ${tempfile}`);
-  const { stderr } = await exec(`sox ${tempfile} -n stat`);
+  await exec(`${ffmpeg} -i ${filename} ${tempfile}`);
+  const { stderr } = await exec(`${sox} ${tempfile} -n stat`);
   const parsed = stderr.match(/RMS[ ]+amplitude:[ ]+([\d.]+)/);
   return parsed && parseFloat(parsed[1]);
 };
